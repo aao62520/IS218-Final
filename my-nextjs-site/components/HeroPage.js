e7ec002f48e481f64e78e0e73346eec11498b271
MikeVillagomez <Mbv23@njit.edu>
2023-12-05 03:23:25 -0500

    hh

--- my-nextjs-site/components/HeroPage.js
+++ my-nextjs-site/components/HeroPage.js
@@ -1,12 +1,12 @@
-// components/HeroPage.js
+// components/HeroPage.js//
 import React from 'react';
-import { Button } from '@nextui-org/react'; // Import only the components you need from Next UI
-import styles from '../styles/Home.module.css'; // Import your home styles
+import { Button } from '@nextui-org/react';
+import styles from '../styles/Home.module.css';
 
 const HeroPage = () => {
   return (
-    <div className={styles.hero}> {/* Use styles.hero for your hero section styles */}
-      <div className={styles.heroContent}> {/* Wrapper for the text content */}
+    <div className={styles.hero}>
+      <div className={styles.heroContent}>
         <h1 className={styles.heroTitle}>
           Where Flavor Meets Tranquility
         </h1>
