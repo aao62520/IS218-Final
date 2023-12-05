2e293d63b18cbc295d6723066bf82da03c533251
MikeVillagomez <Mbv23@njit.edu>
2023-12-05 03:18:53 -0500

    hi

--- my-nextjs-site/next.config.js
+++ my-nextjs-site/next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    basePath: '/my-nextjs-site', // Adjust the path to match your folder structure
+  }
+  
