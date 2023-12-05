15aaa5d4a0151e8b78a5072d77c1f2f901d635b7
MikeVillagomez <Mbv23@njit.edu>
2023-12-05 02:58:14 -0500

    hi

--- my-nextjs-site/components/HeroPage.js
+++ my-nextjs-site/components/HeroPage.js
@@ -1,20 +1,19 @@
 // components/HeroPage.js
 import React from 'react';
-import { Button } from '@nextui-org/react';
-import styles from '../styles/Home.module.css';
+import { Button } from '@nextui-org/react'; // Import only the components you need from Next UI
+import styles from '../styles/Home.module.css'; // Import your home styles
 
 const HeroPage = () => {
   return (
-    <div className={styles.hero}>
-      <div className={styles.heroContent}>
+    <div className={styles.hero}> {/* Use styles.hero for your hero section styles */}
+      <div className={styles.heroContent}> {/* Wrapper for the text content */}
         <h1 className={styles.heroTitle}>
           Where Flavor Meets Tranquility
         </h1>
         <p className={styles.heroSubtitle}>
           Shop for our quality products
         </p>
-        {/* Removed auto and ghost props to avoid conflicts with custom styles */}
-        <Button className={`${styles.heroButton} ${styles.customButton}`}>
+        <Button auto ghost className={styles.heroButton}>
           Shop Now
         </Button>
       </div>
