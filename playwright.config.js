58f4f08ca55dba8f802cb149bab0171b8e023493
Mike Villagomez <114159977+MikeVillagomez@users.noreply.github.com>
2023-12-11 05:21:58 -0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -61,8 +61,8 @@ module.exports = defineConfig({
 
 //  Run your local dev server before starting the tests */
   webServer: {
-  command: 'cd pages && npm run start',
-  url: 'http://127.0.0.1:3000',
+  command: 'npm run dev',
+  url: 'http://localhost:3000',
   reuseExistingServer: !process.env.CI,
 },
 
