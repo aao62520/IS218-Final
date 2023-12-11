ef3ff2bd8009fdebb74df582404ce2bd48f7076c
Mike Villagomez <114159977+MikeVillagomez@users.noreply.github.com>
2023-12-11 04:33:19 -0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,3 @@
-//playwright.config.js
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
 
@@ -22,7 +21,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'github' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -37,16 +36,6 @@ module.exports = defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
     },
 
     /* Test against mobile viewports. */
@@ -66,15 +55,14 @@ module.exports = defineConfig({
     // },
     // {
     //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
+//  Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI,
-  },
+     command: 'npm run start',
+     url: 'http://127.0.0.1:3000',
+     reuseExistingServer: !process.env.CI,
+   },
 });
-
