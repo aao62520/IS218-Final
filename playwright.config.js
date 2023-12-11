571d857e50e1f96df72239831332de0515444b63
MikeVillagomez <Mbv23@njit.edu>
2023-12-11 03:47:45 -0500

    123

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-     baseURL: 'http://127.0.0.1:3000',
+    // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,10 +72,9 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start', // Or 'next start' if that's your start script
-    port: 3000, // Ensure this matches the port your app runs on
-    timeout: 120000, // Timeout to wait for the server to start
-    reuseExistingServer: !process.env.CI
+    command: 'npm run start',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
   },
 });
 
