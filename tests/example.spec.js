215bfeb0596dd2372c30ca2abd0ffc019c7adfd3
MikeVillagomez <Mbv23@njit.edu>
2023-12-11 03:33:37 -0500

    test #2

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,4 +7,9 @@ test('Home page has the correct title', async ({ page }) => {
   await expect(page).toHaveTitle(/Blend-brew Tea/);
 });
 
+test('Home page has correct SEO description', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  const description = await page.getAttribute('meta[name="description"]', 'content');
+  expect(description).toBe("Discover the art of fine tea with Blend-brew Tea. Explore our crafted blends for a symphony of flavors.");
+});
 
