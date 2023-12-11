280edaf223a623686fe20e2fd42563823fc04053
MikeVillagomez <Mbv23@njit.edu>
2023-12-11 01:46:01 -0500

    navbar remove shop us

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,4 +1,3 @@
-// components/NavigationBar.js
 import React from 'react';
 import styles from '../styles/NavigationBar.module.css';
 import { Navbar, Link } from '@nextui-org/react';
@@ -9,10 +8,10 @@ const NavigationBar = () => {
       <span className={styles.brand}>Blend-brew Tea</span>
       <nav>
         <ul className={styles.menu}>
-          <li><Link href="/home" className={styles.menuItem}>Home</Link></li>
-          <li><Link href="/shop" className={styles.menuItem}>Shop</Link></li>
-          <li><Link href="/about" className={styles.menuItem}>About Us</Link></li>
-          <li><Link href="/signup" className={styles.menuItem}>Sign Up/Login</Link></li>
+          <li><Link href="#top" className={styles.menuItem}>Home</Link></li> {/* Assuming 'Home' refers to the top of the page */}
+          {/* 'Shop' link removed */}
+          <li><Link href="#footer" className={styles.menuItem}>About Us</Link></li> {/* Scroll to Footer */}
+          <li><Link href="#signup" className={styles.menuItem}>Sign Up</Link></li> {/* Scroll to Email Input Section */}
         </ul>
       </nav>
     </Navbar>
