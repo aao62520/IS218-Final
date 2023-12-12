496406d9595082f50450acdc210608d3a2aecf83
MikeVillagomez <Mbv23@njit.edu>
2023-12-11 20:19:24 -0500

    mailchimp

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -13,5 +13,17 @@ test('Home page has correct SEO description', async ({ page }) => {
   expect(description).toBe("Discover the art of fine tea with Blend-brew Tea. Explore our crafted blends for a symphony of flavors.");
 });
 
+test('Navigation bar works correctly', async ({ page }) => {
+  await page.goto('http://localhost:3000');
 
+  // Close the Mailchimp modal if it's present
+  const mailchimpModalCloseButton = page.locator('.mc-closeModal');
+  if (await mailchimpModalCloseButton.isVisible()) {
+    await mailchimpModalCloseButton.click();
+  }
+
+  // Now click the navigation link
+  await page.click('nav >> text=About Us');
+  // Add further assertions as needed
+});
 
