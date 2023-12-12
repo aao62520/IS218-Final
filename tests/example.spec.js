902075a6470d4f068309bd6bcb617e092ca909c2
MikeVillagomez <Mbv23@njit.edu>
2023-12-11 20:07:55 -0500

    1

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,29 +1,16 @@
-//example.spec.js
 const { test, expect } = require('@playwright/test');
 
+// Test for the Home page title
 test('Home page has the correct title', async ({ page }) => {
   await page.goto('http://localhost:3000');
   await expect(page).toHaveTitle(/Blend-brew Tea/);
 });
 
+// Test for SEO description on the Home page
 test('Home page has correct SEO description', async ({ page }) => {
   await page.goto('http://localhost:3000');
   const description = await page.getAttribute('meta[name="description"]', 'content');
   expect(description).toBe("Discover the art of fine tea with Blend-brew Tea. Explore our crafted blends for a symphony of flavors.");
-});
-
-test('Navigation bar works correctly', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-
-  // Close the modal if it's present
-  const modalCloseButton = page.locator('button:has-text("Close")'); // Replace with the actual selector for closing the modal
-  if (await modalCloseButton.isVisible()) {
-    await modalCloseButton.click();
-  }
-
-  // Now click the navigation link
-  await page.click('nav >> text=About Us');
-  // Add assertions as needed
 });
 
 
