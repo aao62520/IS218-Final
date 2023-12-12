dcaf66a8b33f9536428bbbb274402b8177c174c0
MikeVillagomez <Mbv23@njit.edu>
2023-12-11 19:36:36 -0500

    changes

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -11,3 +11,12 @@ test('Home page has correct SEO description', async ({ page }) => {
   const description = await page.getAttribute('meta[name="description"]', 'content');
   expect(description).toBe("Discover the art of fine tea with Blend-brew Tea. Explore our crafted blends for a symphony of flavors.");
 });
+
+test('Footer section has correct content', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  // Check for specific text in the footer
+  await expect(page.locator('footer #footer')).toContainText('About Blend-brew Tea');
+  await expect(page.locator('footer #footer')).toContainText('FAQ');
+  await expect(page.locator('footer #footer')).toContainText('Contact Us');
+});
