ec0f095a457dae054a4e2e7a66061d53e857d0f7
MikeVillagomez <Mbv23@njit.edu>
2023-12-18 18:51:45 -0500

    add 123

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/globals.css';
 import Head from 'next/head';
-import Script from 'next/script';
 import NavigationBar from '../components/NavigationBar';
 import CookieConsent from 'react-cookie-consent';
-import ReactGA from 'react-ga';
 import { Link } from '@nextui-org/react';
 
 function MyApp({ Component, pageProps }) {
   const [cookieConsent, setCookieConsent] = useState(false);
 
   useEffect(() => {
-    // Check if the consent cookie is set
     const consent = localStorage.getItem('cookieName') === 'true';
     setCookieConsent(consent);
-
     if (consent) {
-      // Initialize Google Analytics only if consent is given
-      ReactGA.initialize('G-XDWMNHNFNE');
-      ReactGA.pageview(window.location.pathname + window.location.search);
+      loadGoogleAnalytics();
     }
   }, []);
+
+  const loadGoogleAnalytics = () => {
+    const script = document.createElement('script');
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-XDWMNHNFNE";
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', 'G-XDWMNHNFNE');
+    };
+  };
 
   const handleAcceptCookie = () => {
     setCookieConsent(true);
     localStorage.setItem('cookieName', 'true');
-    // Initialize Google Analytics upon accepting
-    ReactGA.initialize('G-XDWMNHNFNE');
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    loadGoogleAnalytics();
   };
 
   const handleDeclineCookie = () => {
     setCookieConsent(false);
     localStorage.setItem('cookieName', 'false');
-    // Additional logic if you need to disable analytics when cookies are declined
+    // Additional logic if needed to disable analytics when cookies are declined
   };
 
   return (
@@ -46,26 +52,8 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:description" content="Explore our crafted blends for a symphony of flavors." />
         <meta property="og:image" content="/happy1.jpg" />
         <meta property="og:type" content="website" />
-        <link rel="icon" href="/Brew.png" />      
+        <link rel="icon" href="/Brew.png" />    
       </Head>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-XDWMNHNFNE"
-        strategy="afterInteractive"
-        async
-        data-testid="google-analytics-script"
-      ></Script>
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        data-testid="google-analytics-inline-script"
-      >
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-XDWMNHNFNE');
-        `}
-      </Script>
       <NavigationBar />
       <Component {...pageProps} />
       <CookieConsent
--- pages/layout.js
+++ pages/layout.js
@@ -1,5 +1,3 @@
-// Layout.js
-
 import React from 'react';
 import Head from 'next/head';
 
@@ -7,22 +5,8 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
-        {/* ... Your other meta tags, title, and other head content ... */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-XDWMNHNFNE"
-        ></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-              dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-
-            gtag('config', 'G-XDWMNHNFNE');
-          `}
-        </script>
+        {/* Your other meta tags, title, and head content here */}
+        {/* Removed Google Analytics script for consent compliance */}
         {/* ... Additional meta tags or scripts ... */}
       </Head>
       {children}
