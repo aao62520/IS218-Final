6fae7b935bea080bbb72c03e1e46aa90c97b7c46
MikeVillagomez <Mbv23@njit.edu>
2023-12-18 18:28:21 -0500

    fixed cookie consent

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
-// pages/_app.js
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/globals.css';
 import Head from 'next/head';
 import Script from 'next/script';
@@ -9,23 +8,45 @@ import ReactGA from 'react-ga';
 import { Link } from '@nextui-org/react';
 
 function MyApp({ Component, pageProps }) {
+  const [cookieConsent, setCookieConsent] = useState(false);
+
   useEffect(() => {
-    // Initialize Google Analytics
+    // Check if the consent cookie is set
+    const consent = localStorage.getItem('cookieName') === 'true';
+    setCookieConsent(consent);
+
+    if (consent) {
+      // Initialize Google Analytics only if consent is given
+      ReactGA.initialize('G-XDWMNHNFNE');
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  }, []);
+
+  const handleAcceptCookie = () => {
+    setCookieConsent(true);
+    localStorage.setItem('cookieName', 'true');
+    // Initialize Google Analytics upon accepting
     ReactGA.initialize('G-XDWMNHNFNE');
     ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+  };
+
+  const handleDeclineCookie = () => {
+    setCookieConsent(false);
+    localStorage.setItem('cookieName', 'false');
+    // Additional logic if you need to disable analytics when cookies are declined
+  };
 
   return (
     <>
       <Head>
-        <title>Blend-brew Tea</title>
+      <title>Blend-brew Tea</title>
         <meta name="description" content="Discover the art of fine tea with Blend-brew Tea. Explore our crafted blends for a symphony of flavors." />
         <meta name="keywords" content="tea, blendbrew tea, organic tea, herbal tea, green tea, black tea, loose leaf tea, tea blends, specialty tea, artisan tea" />
         <meta property="og:title" content="Blend-brew Tea" />
         <meta property="og:description" content="Explore our crafted blends for a symphony of flavors." />
         <meta property="og:image" content="/happy1.jpg" />
         <meta property="og:type" content="website" />
-        <link rel="icon" href="/Brew.png" />
+        <link rel="icon" href="/Brew.png" />      
       </Head>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-XDWMNHNFNE"
@@ -54,10 +75,12 @@ function MyApp({ Component, pageProps }) {
         enableDeclineButton
         declineButtonText="Decline"
         declineCookieValue={false}
-        style={{ background: "#2B373B" }}  // Changed color as requested
-        buttonStyle={{ background:"#007BFF", color: "white", fontSize:"15px"}} // Adjusted for better visibility and contrast
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ background:"#007BFF", color: "white", fontSize:"15px"}}
         declineButtonStyle={{ background:"#cfcfcf", color: "black", fontSize: "15px"}}
         expires={150}
+        onAccept={handleAcceptCookie}
+        onDecline={handleDeclineCookie}
       >
         This website uses cookies to improve your experience. By using our website, you agree to our 
         <Link href="/policy" style={{ color: "#F1D00A" }}>Privacy Policy</Link>.
